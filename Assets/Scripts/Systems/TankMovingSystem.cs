128a976d2e733fbee0b4469a6dfbb96417db072c
Dominik Gałenza <dominik.galenza@gmail.com>
2023-02-06 21:38:39 +0100

    Implemented tank movement.

---
+++ Assets/Scripts/Systems/TankMovingSystem.cs
@@ -0,0 +1,25 @@
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+partial class TankMovingSystem : SystemBase
+{
+    protected override void OnUpdate()
+    {
+        float deltaTime = SystemAPI.Time.DeltaTime;
+
+        Entities
+            .WithAll<Tank>()
+            .ForEach((TransformAspect transform) =>
+            {
+                float3 position = transform.LocalPosition;
+
+                float angle = (0.5f + noise.cnoise(position / 10f)) * 4f * math.PI;
+
+                float3 direction = float3.zero;
+                math.sincos(angle, out direction.x, out direction.z);
+                transform.LocalPosition += direction * deltaTime * 5f;
+                transform.LocalRotation = quaternion.RotateY(angle);
+            }).ScheduleParallel();
+    }
+}
